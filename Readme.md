e59efc65b41f2fa549b30a34d7942932fdc1983f
Saravanan <saravananselvamani@gmail.com>
2022-11-03 08:53:12 +0530

    returning headers

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Installation
+1. install poetry
+
+## Run app
+```
+poetry shell
+uvicorn main:app --reload
+```
+
--- routers/reconcile.py
+++ routers/reconcile.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, File, UploadFile
 import pandas as pd
+import csv
 
 router = APIRouter(prefix='/reconcile')
 
@@ -9,9 +10,19 @@ def reconcile():
 
 @router.post('/uploadfile')
 async def uploadfile(file1: UploadFile, file2: UploadFile):
-    csv_file1 = pd.read_csv(file1.file)
-    csv_file2 = pd.read_csv(file2.file)
+    has_header1 = has_header2 = False
+    has_header1 = csv.Sniffer().has_header(str(file1.file.read(2048)))
+    has_header2 = csv.Sniffer().has_header(str(file2.file.read(2048)))
+    file1.file.seek(0)
+    file2.file.seek(0)
+    csv_file1 = pd.read_csv(file1.file, header= (0 if has_header1 else None))
+    csv_file2 = pd.read_csv(file2.file, header=(0 if has_header2 else None ))
     print(csv_file1)
     print(csv_file2)
-    # print(file1_content)
-    return {'file1': file1.filename, 'file2': file2.filename}
+    return {'file1': {
+        'name': file1.filename,
+        'headers': list(csv_file1.columns)
+    }, 'file2': {
+        'name': file2.filename,
+        'headers': list(csv_file2.columns)
+    }}
